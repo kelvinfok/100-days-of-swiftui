b8d453fd9e5034fe84262cb2b708e3d0cca7ed48
kelvinfok <punkd1@gmail.com>
2024-08-12 21:29:44 +0800

    add day 16

---
+++ day16/day16/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  day16
+//
+//  Created by Kelvin Fok on 12/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ day16/day16/day16App.swift
@@ -0,0 +1,17 @@
+//
+//  day16App.swift
+//  day16
+//
+//  Created by Kelvin Fok on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct day16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
